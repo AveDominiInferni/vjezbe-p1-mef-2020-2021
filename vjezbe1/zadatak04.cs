5bfe9593b9677098cd1fc7b942549a17418b6615
ave_satanas <33603067+AveSatanasBiH@users.noreply.github.com>
2020-12-06 19:08:31 +0100

    Create zadatak04.cs

---
+++ vjezbe1/zadatak04.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace vjezbe1
+{ 
+    class zadatak
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("\nWelcome " + name + "!");
+        }
+    }
+}
