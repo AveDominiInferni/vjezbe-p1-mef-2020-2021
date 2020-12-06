0e95fb69fa6be342f1754a537d0a90379ec7f465
ave_satanas <33603067+AveSatanasBiH@users.noreply.github.com>
2020-12-07 00:45:35 +0100

    Create zadatak13.cs

---
+++ vjezbe3/zadatak13.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+
+namespace vjezbe3
+{ 
+    class zadatak
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite koordinate tacke A");
+            Console.Write("xA = ");
+            double xA = Convert.ToDouble(Console.ReadLine());
+            Console.Write("yA = ");
+            double yA = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\nTacka A({0};{1}) se nalazi ", xA, yA);
+            if (xA > 0 && yA > 0)
+                Console.Write("u I kvadrantu.");
+            else if (xA < 0 && yA > 0)
+                Console.Write("u II kvadrantu.");
+            else if (xA < 0 && yA < 0)
+                Console.Write("u III kvadrantu.");
+            else if (xA > 0 && yA < 0)
+                Console.Write("u IV kvadrantu.");
+            else if (xA == 0 && yA != 0)
+                Console.Write("na y osi.");
+            else if (xA != 0 && yA == 0)
+                Console.Write("na x osi.");
+            else
+                Console.Write("na u koordinatnom pocetku.");
+        }
+    }
+}
