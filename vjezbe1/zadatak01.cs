329726341bef94676d67e0d54a076fbc7db36455
ave_satanas <33603067+AveSatanasBiH@users.noreply.github.com>
2020-12-06 19:00:13 +0100

    Create zadatak01.cs

---
+++ vjezbe1/zadatak01.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace vjezbe1
+{ 
+    class zadatak
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Opseg long tipa podataka je od {0} do {1}", long.MinValue, long.MaxValue);
+        }
+    }
+}
