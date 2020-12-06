f1c97a918aeef964e14d8de1b31ed1a77c11b3d9
ave_satanas <33603067+AveSatanasBiH@users.noreply.github.com>
2020-12-06 19:28:36 +0100

    Create zadatak01.cs

---
+++ vjezbe3/zadatak01.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace vjezbe3
+{ 
+    class zadatak
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite jedan cijeli broj");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n > 0)
+                Console.WriteLine("\nBroj {0} je pozitivan.", n);
+            else if (n < 0)
+                Console.WriteLine("\nBroj {0} je negativan.", n);
+            else
+                Console.WriteLine("\nBroj je 0.");
+        }
+    }
+}
