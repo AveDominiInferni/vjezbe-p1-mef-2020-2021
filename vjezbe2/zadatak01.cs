f69f53246aa7d62b025302eda30ab93959373b69
ave_satanas <33603067+AveSatanasBiH@users.noreply.github.com>
2020-12-06 19:12:23 +0100

    Create zadatak01.cs

---
+++ vjezbe2/zadatak01.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace vjezbe1
+{ 
+    class zadatak
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            double z = 15 + a / 2 - 2.5;
+            Console.WriteLine("Rezultat je {0}", z);
+        }
+    }
+}
